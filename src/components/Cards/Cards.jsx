95faf55459f4b392dfe2a1941b336cf8c4c9543a
swapno92 <swapnoprograming92@gmaiil.com>
2023-09-15 02:00:30 +0600

    data is dynamically show on each card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Card.css'
 import Courses from '../Courses/Courses';
 import PurchasedCourses from '../PurchasedCourses/PurchasedCourses';
 
 const Cards = () => {
+    const [courses, setCourses] = useState([])
+
+    useEffect(() => {
+        fetch('courses.json')
+            .then(res => res.json())
+            .then(data => setCourses(data))
+    }, [])
+    // console.log(courses);
+
     return (
         <div className=' flex gap-4'>
-            <Courses></Courses>
+            <div className='w-3/4 grid grid-cols-3 gap-4'>
+                {
+                    courses.map(course => <Courses key={course.id} course={course}></Courses>)
+                }
+            </div>
+
+            {/* <Courses></Courses> */}
             <PurchasedCourses></PurchasedCourses>
         </div>
     );
